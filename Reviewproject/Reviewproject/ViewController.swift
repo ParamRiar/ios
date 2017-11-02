e5ffa2cc249f1e0e20be8040c1033a58b419b6a3
Param <32308577+ParamRiar@users.noreply.github.com>
2017-11-02 14:48:15 -0400

    Add files via upload

---
+++ Reviewproject/Reviewproject/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Reviewproject
+//
+//  Created by MacStudent on 2017-11-02.
+//  Copyright © 2017 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func btnclick(_ sender: UIButton) {
+        print("hello")
+        print("bye")
+    }
+
+    
+    
+}
+
